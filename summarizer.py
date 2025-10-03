e172a87d9226ec3f5812c891394d4f29f7858352
Kevil Rana <125282403+Kevv28@users.noreply.github.com>
2025-10-03 12:29:42 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
+GROQ_API_KEY =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
 def summarize_text(text: str) -> str:
     url = "https://api.groq.com/openai/v1/chat/completions"
     
